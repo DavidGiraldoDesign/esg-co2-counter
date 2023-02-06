6752b3e39478dbec93030502b53dfc2411f005af
David Giraldo M <davidgiraldom@Davids-MacBook-Pro.local>
2023-02-05 19:25:58 -0600

    Adding chart and equivalencies layout

---
+++ public/Chart.js
@@ -0,0 +1,20 @@
+const ctx = document.getElementById('myPieChart');
+
+const data = {
+    datasets: [{
+        data: [300, 50],
+        borderColor:'rgba(0,0,0,0)',
+        backgroundColor: [
+            'rgba(244, 174, 60, 1)',
+            'rgb(0,0,0,0)'
+        ],
+        hoverOffset: 4
+    }]
+};
+
+const config = {
+    type: 'pie',
+    data: data,
+};
+
+new Chart(ctx, config);
